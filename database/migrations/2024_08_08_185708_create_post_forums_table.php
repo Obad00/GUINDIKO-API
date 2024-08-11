0899f198a55c33495eb2db4a3af83f10678faa5b
mamebassine <mamebassine06@gmail.com>
2024-08-11 02:00:50 +0000

    gestions notifications

--- database/migrations/2024_08_08_185708_create_post_forums_table.php
+++ database/migrations/2024_08_08_185708_create_post_forums_table.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Models\User;
 use App\Models\Forum;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string('contenu');
             $table->string('image');
             $table->foreignIdFor(Forum::class)->onDelete('cascade');
+            $table->foreignIdFor(User::class)->onDelete('cascade');
             $table->timestamps();
         });
     }
