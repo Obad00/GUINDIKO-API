a0ea12abbb880521bf8d455c20b2180181e22982
Adama Dabo <dabodabo9064@gmail.com>
2024-08-12 01:25:39 +0000

    Récuperation des derniers traveaux

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         $this->call([
             UserSeeder::class,
             ForumSeeder::class
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
