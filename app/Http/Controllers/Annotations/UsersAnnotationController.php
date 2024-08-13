a35fd46304c129d386558f3170923893bee4d10c
Adama Dabo <dabodabo9064@gmail.com>
2024-08-13 20:13:22 +0000

    test

---
+++ app/Http/Controllers/Annotations/UsersAnnotationController.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Http\Controllers\Annotations ;
+
+/**
+ * @OA\Security(
+ *     security={
+ *         "BearerAuth": {}
+ *     }),
+
+ * @OA\SecurityScheme(
+ *     securityScheme="BearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"),
+
+ * @OA\Info(
+ *     title="Your API Title",
+ *     description="Your API Description",
+ *     version="1.0.0"),
+
+ * @OA\Consumes({
+ *     "multipart/form-data"
+ * }),
+
+ *
+
+ * @OA\GET(
+ *     path="/api/users",
+ *     summary="Lister les utilisateurs",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"Users"},
+*),
+
+
+ * @OA\POST(
+ *     path="/api/users",
+ *     summary="Ajouter un mentor",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="201", description="Created successfully"),
+ * @OA\Response(response="400", description="Bad Request"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="nom", type="string"),
+ *                     @OA\Property(property="prenom", type="string"),
+ *                     @OA\Property(property="numeroTelephone", type="string"),
+ *                     @OA\Property(property="email", type="string"),
+ *                     @OA\Property(property="password", type="string"),
+ *                     @OA\Property(property="password_confirmation", type="string"),
+ *                     @OA\Property(property="role", type="string"),
+ *                     @OA\Property(property="domaineExpertise", type="string"),
+ *                     @OA\Property(property="experience", type="string"),
+ *                     @OA\Property(property="disponibilite", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Users"},
+*),
+
+
+ * @OA\DELETE(
+ *     path="/api/users/{id}",
+ *     summary="Supprimer un utilisateur",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="204", description="Deleted successfully"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ * @OA\Response(response="404", description="Not Found"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="nom", type="string"),
+ *                     @OA\Property(property="prenom", type="string"),
+ *                     @OA\Property(property="numeroTelephone", type="string"),
+ *                     @OA\Property(property="email", type="string"),
+ *                     @OA\Property(property="password", type="string"),
+ *                     @OA\Property(property="password_confirmation", type="string"),
+ *                     @OA\Property(property="role", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Users"},
+*),
+
+
+ * @OA\PUT(
+ *     path="/api/users/{id}",
+ *     summary="Modifier un utilisateur",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/x-www-form-urlencoded",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="nom", type="string"),
+ *                     @OA\Property(property="prenom", type="string"),
+ *                     @OA\Property(property="numeroTelephone", type="string"),
+ *                     @OA\Property(property="email", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Users"},
+*),
+
+
+ * @OA\PATCH(
+ *     path="/api/users/{id}/activation",
+ *     summary="Activation ou désactivation",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="path", name="id", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/x-www-form-urlencoded",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="is_active", type="boolean"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Users"},
+*),
+
+
+*/
+
+ class UsersAnnotationController {}
