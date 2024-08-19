a059fc4ba22255cb493494b3c5e24bf91f9712f4
Adama Dabo <dabodabo9064@gmail.com>
2024-08-19 17:25:04 +0000

    Mise à jour de la documentation

--- app/Http/Controllers/Annotations/PermissionsAnnotationController.php
+++ app/Http/Controllers/Annotations/PermissionsAnnotationController.php
@@ -26,6 +26,51 @@ namespace App\Http\Controllers\Annotations ;
  *
 
  * @OA\GET(
+ *     path="/api/roles/2/permissions",
+ *     summary="Véerifier les permissions d'un role",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="200", description="OK"),
+ * @OA\Response(response="404", description="Not Found"),
+ * @OA\Response(response="500", description="Internal Server Error"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     tags={"Permissions"},
+*),
+
+
+ * @OA\POST(
+ *     path="/api/roles/{id}/assign-permission",
+ *     summary="Assigner permission à role",
+ *     description="",
+ *         security={
+ *    {       "BearerAuth": {}}
+ *         },
+ * @OA\Response(response="201", description="Created successfully"),
+ * @OA\Response(response="400", description="Bad Request"),
+ * @OA\Response(response="401", description="Unauthorized"),
+ * @OA\Response(response="403", description="Forbidden"),
+ *     @OA\Parameter(in="header", name="User-Agent", required=false, @OA\Schema(type="string")
+ * ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="multipart/form-data",
+ *             @OA\Schema(
+ *                 type="object",
+ *                 properties={
+ *                     @OA\Property(property="permission", type="string"),
+ *                 },
+ *             ),
+ *         ),
+ *     ),
+ *     tags={"Permissions"},
+*),
+
+
+ * @OA\GET(
  *     path="/api/permissions",
  *     summary="Lister les permissions",
  *     description="",
